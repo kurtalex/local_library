72dc5c010bfb51654e72c9fb11726175d2d4531a
kurt_alex <kurt_alex@mail.ru>
2018-08-26 21:22:14 +0300

    create admin-panel and add default models

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.contrib import admin
+from .models import Author, Genre, Book, BookInstance
 
-# Register your models here.
+admin.site.register(Book)
+admin.site.register(Author)
+admin.site.register(Genre)
+admin.site.register(BookInstance)
